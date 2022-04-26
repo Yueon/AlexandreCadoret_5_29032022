56b54890dea36de385ae4001373898b32954c6a1
Yueon <cado5alexandre@gmail.com>
2022-04-26 17:10:55 +0200

    productajoutpanier

--- front/JS/product.js
+++ front/JS/product.js
@@ -2,13 +2,14 @@ import { getProduct } from "./api.js";
 
 console.log("window Location:", window.location);
 
+let product
 //On récupère l'ID du produit
 const paramsUrl = new URLSearchParams(window.location.search)
   const urlId = paramsUrl.get('id')
   console.log(urlId)
 
 window.onload = async () => {
-  const product = await getProduct(urlId)
+  product = await getProduct(urlId)
   console.log(product)
   createElements(product)
 }
@@ -21,7 +22,7 @@ function createElements(product){
     const priceHtml = document.getElementById('price');
     const descriptionHtml = document.getElementById('description');
 
-    imgHtml.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"/>`;
+    imgHtml[0].innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"/>`;
     titleHtml.innerHTML += `${product.name}`;
     priceHtml.innerHTML += `${product.price}`;
     descriptionHtml.innerHTML += `${product.description}`;
@@ -33,3 +34,50 @@ function createElements(product){
       optionHtml.innerHTML += `<option value="${options}">${options}</option>`
       })
 }
+
+
+/* 1-sur clic de ajout au panier save les données (id,couleur,nombre) dans un array en localstorage
+    2- si ont ajoute un canap qui n'était pas dans le array (même id + même couleur) 
+    on rajoute l'élément, sinon on incrémente juste la quantité du produit correspondant.
+
+      1-créer fonction event click
+      2-créer object (id, couleur, nombre)
+      3-créer fonction ajout object dans localstorage
+      4-créer fonction if,else check localstorage
+    */
+
+
+    document
+        .getElementById('addToCart')
+        .addEventListener("click", function(Event){
+          console.log(Event)
+          let optionColor = document.getElementById('colors').value
+          console.log('couleur choisie :', optionColor)
+          let optionQuantity = document.getElementById('quantity').value
+          console.log('quantité choisie :', optionQuantity)
+
+          if(optionQuantity > 0 && optionColor.length > 0) {
+            console.log('produit ajouté au panier')
+          }else {
+            console.log('quantité non sélectionné')
+          }
+
+          let objectProduct = {
+            id: product._id,
+            colors: optionColor,
+            Number: optionQuantity,
+          }
+          console.log('produit ajouté au panier :', objectProduct);
+});
+
+/*
+const i = cart.findIndex((item) => item.productId === productId && item.productColor === productColor)
+
+if(i > -1) {
+  console.log("Déjà dans le panier, ajustement")
+  } else {
+  console.log("Nouveau produit dans le panier")
+})
+}
+
+*/
